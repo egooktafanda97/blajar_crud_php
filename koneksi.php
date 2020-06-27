98f8311992023cd15dfd5760ac6fd20fdd0c893f
egooktafanda97 <48469529+egooktafanda97@users.noreply.github.com>
2020-06-28 03:04:44 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -6,6 +6,11 @@ require_once("lib/config.php");
 $Config = new Config();
 
 // koneksi  
+// koneksi  
+//nama database latuhan_crud
+//host database jika memakai xampp pakai localhost
+//username database untuk xampp biasanya root
+//password default xampp biasanya kosong
 $data = ["latuhan_crud","localhost","root",""];
 $Config->setDatabase($data);
 // --------------------------------------------
